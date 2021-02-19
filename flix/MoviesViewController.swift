ddc9b7d2f75b6e89816c046b8d28a8079ca71dca
Pushpal Patil <gauripushp@gmail.com>
2021-02-18 23:28:33 -0800

    flix pt.2

--- flix/MoviesViewController.swift
+++ flix/MoviesViewController.swift
@@ -76,14 +76,27 @@ class MoviesViewController: UIViewController, UITableViewDataSource, UITableView
         
         // used pod file, opened it, inserted AlamofireImage, imported it in this file
     }
-    /*
+    
     // MARK: - Navigation
 
     // In a storyboard-based application, you will often want to do a little preparation before navigation
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
         // Get the new view controller using segue.destination.
+        
+        // find the selected movie - sender is the cell that was tapped on
+        let cell = sender as! UITableViewCell
+        let indexPath = tableView.indexPath(for: cell)!
+        let movie = movies[indexPath.row]
+        
+        
         // Pass the selected object to the new view controller.
+        
+        let detailsViewController = segue.destination as! MovieDetailsViewController
+        detailsViewController.movie = movie
+        tableView.deselectRow(at: indexPath, animated: true)
+        
+        
     }
-    */
+    
 
 }
